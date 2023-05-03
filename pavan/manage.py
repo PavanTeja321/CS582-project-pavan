640f91f9ba05a37c04eb974b0a3d2856f5027416
v578q722 <123615356+PavanTeja321@users.noreply.github.com>
2023-05-03 17:23:21 -0500

    Update manage.py

--- pavan/manage.py
+++ pavan/manage.py
@@ -60,3 +60,14 @@ class ArticleModelForm(forms.ModelForm):
             'content',
             'active',
         ]
+
+from django.db import models
+from django.urls import reverse
+
+class Article(models.Model):
+    title   = models.CharField(max_length=120)
+    content = models.TextField()
+    active  = models.BooleanField(default=True)
+
+    def get_absolute_url(self):
+        return reverse("articles:article-detail", kwargs={"id": self.id})
