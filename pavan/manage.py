c260d57fff84a54a4df0a1defc50199a4f815ef8
v578q722 <123615356+PavanTeja321@users.noreply.github.com>
2023-05-03 17:40:08 -0500

    Update manage.py

--- pavan/manage.py
+++ pavan/manage.py
@@ -458,3 +458,8 @@ from django.contrib import admin
 from .models import Product
 
 admin.site.register(Product)
+
+from django.apps import AppConfig
+
+class ProductsConfig(AppConfig):
+    name = 'products'
