8875f9afb67b49b649cf079ae0b2722eca04d300
v578q722 <123615356+PavanTeja321@users.noreply.github.com>
2023-05-03 17:26:35 -0500

    Update manage.py

--- pavan/manage.py
+++ pavan/manage.py
@@ -123,3 +123,33 @@ class ArticleListView(ListView):
     template_name = 'articles/article_list.html'
     queryset = Article.objects.all() # <blog>/<modelname>_list.html
 
+class ArticleDetailView(DetailView):
+    template_name = 'articles/article_detail.html'
+    #queryset = Article.objects.all()
+
+    def get_object(self):
+        id_ = self.kwargs.get("id")
+        return get_object_or_404(Article, id=id_)
+
+class ArticleUpdateView(UpdateView):
+    template_name = 'articles/article_create.html'
+    form_class = ArticleModelForm
+
+    def get_object(self):
+        id_ = self.kwargs.get("id")
+        return get_object_or_404(Article, id=id_)
+
+    def form_valid(self, form):
+        print(form.cleaned_data)
+        return super().form_valid(form)
+
+
+class ArticleDeleteView(DeleteView):
+    template_name = 'articles/article_delete.html'
+    
+    def get_object(self):
+        id_ = self.kwargs.get("id")
+        return get_object_or_404(Article, id=id_)
+
+    def get_success_url(self):
+        return reverse('articles:article-list')
